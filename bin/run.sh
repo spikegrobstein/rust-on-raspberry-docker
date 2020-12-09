c637fdb872e0e885cefe244a47c23eeff04a1400
Spike Grobstein <me@spike.cx>
2020-12-08 19:34:04 -0800

    basic cleanup of sh script
    no trailing semicolons and use a more portable shbang

--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#! /usr/bin/env/bash
 
 # This script is the entrypoint in the docker container.
 
-set -ex;
-export SYSROOT="$HOME/pi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot";
+set -ex
+export SYSROOT="$HOME/pi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot"
 
-printf "*** Extracting target dependencies ***\n";
+printf "*** Extracting target dependencies ***\n"
 if [ -d "$HOME/deb-deps" ]; then
 
   for i in `find $HOME/deb-deps -name "*.deb" -type f`; do
-    echo "Extracting: $i in `pwd`";
-    ar -p $i data.tar.xz > "$i.tar.xz";
+    echo "Extracting: $i in `pwd`"
+    ar -p $i data.tar.xz > "$i.tar.xz"
     tar x -C $SYSROOT -f  "$i.tar.xz"
   done
 fi
@@ -20,22 +20,22 @@ if [ -x "$HOME/deb-deps/post-install.sh" ] ; then
   source "$HOME/deb-deps/post-install.sh" $SYSROOT $HOME/deb-deps
 fi
 
-printf "\n*** Cross compiling project ***\n";
-cd $HOME/project;
+printf "\n*** Cross compiling project ***\n"
+cd $HOME/project
 
 if [ $(uname -m) == 'x86_64' ]; then
-	TOOLCHAIN=$TOOLCHAIN_64;
+	TOOLCHAIN=$TOOLCHAIN_64
 else
-	TOOLCHAIN=$TOOLCHAIN_32;
+	TOOLCHAIN=$TOOLCHAIN_32
 fi
 
 #Include the cross compilation binaries
-export PATH=$TOOLCHAIN:$PATH;
+export PATH=$TOOLCHAIN:$PATH
 
 # Setup environment variables for cross compilation.
 # Note that cargo needs to compile build.rs for the local host and then the final library or bin for
 # the target host.
 export CC_arm_unknown_linux_gnueabihf="$TOOLCHAIN/gcc-sysroot"
 
-flags="--target=arm-unknown-linux-gnueabihf";
-$HOME/.cargo/bin/cargo "$@" $flags;
+flags="--target=arm-unknown-linux-gnueabihf"
+$HOME/.cargo/bin/cargo "$@" $flags
